0ea068fb8596a5a79256c89725bf13489b3ce93d
Punit <punit_lohani@yahoo.com>
2022-11-23 00:17:31 -0500

    chore(App): add types

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,21 @@ import { Container } from "react-bootstrap"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { NewNote } from "./components/NewNote"
 
+export type Note = {
+  id: string
+} & NoteData
+
+export type NoteData = {
+  title:string
+  markdown: string
+  tags: Tag[]
+}
+
+export type Tag = {
+  id: string
+  label: string
+}
+
 function App() {
   return (
     <Container className="my-4">
